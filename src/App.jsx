56477a55dfa9cb37db7d92d55a4d0602be4f6a42
Abhishek Bharti <abhibharti457@gmail.com>
2025-12-06 17:59:12 +0530

    setup file

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React  from "react";
+
+export default function CancerAwarenessPage() {
+ 
+
+  return (
+   <div className=" bg-black text-5xl text-blue-100">
+    Cancer-Awareness-Support
+   </div>
+  );
+}
